16e14fd89f97f89f29ca8ea803cb7c1f2e13c018
Wasiu Bakare <wasiubakare@gmail.com>
2021-08-05 17:38:30 +0100

    solved palindromes

---
+++ Assignment_1/Answers/Programs/Palindrome/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string word = "Wasiu";
+            static void IsPalindrome(string word)
+            {
+                string wordReversed;
+                for (int i = word.Length - 1; i >= 0 ; i--)
+                {
+                    wordReversed += word[i];
+                }
+                Console.WriteLine(wordReversed);
+            } 
+            Console.WriteLine("Hello World!");
+            IsPalindrome(word);*/
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+            static void IsPalindrome(string word)
+            {
+                for (int i = 0; i < word.Length/2; i++)
+                {
+                    if (word[i] != word[word.Length-1-i])
+                    {
+                        Console.WriteLine("The word is not a palindrome");
+                    } else
+                    {
+                        Console.WriteLine("The word is a palindrome");
+                    }
+                }
+            }
+            IsPalindrome(word);
+        }
+    }
+}
